b61fbec2a002833c8024a60b5233cf4d4f7bc838
Paulo23 <paulo.a23@outlook.com>
2015-10-16 21:33:43 -0300

    Correção para inserção de doação

--- doaweb/src/main/java/br/unifor/pin/doaweb/dao/DoacaoDAO.java
+++ doaweb/src/main/java/br/unifor/pin/doaweb/dao/DoacaoDAO.java
@@ -1,19 +1,26 @@
 package br.unifor.pin.doaweb.dao;
 
 import javax.persistence.EntityManager;
+
 import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.unifor.pin.doaweb.entity.Doacao;
 
+@Repository
+@Transactional(propagation = Propagation.REQUIRED)
 public class DoacaoDAO {
 
 	@PersistenceContext
 	private EntityManager entityManager;
 
-	public void salvar(Doacao doacao){
+	public void salvar(Doacao doacao) {
 		entityManager.persist(doacao);
 	}
-	
+
 	public void atualizar(Doacao doacao) {
 		entityManager.merge(doacao);
 	}
@@ -21,5 +28,5 @@ public class DoacaoDAO {
 	public void excluir(Doacao doacao) {
 		entityManager.remove(doacao);
 	}
-	
+
 }
