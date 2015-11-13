94bb5ec18d00917a9427f87f4da2a57d22b10a0a
Berg Torres <bergtorresn@outlook.com>
2015-11-12 22:01:51 -0300

    [bergtorres] - última tela(informações sobre a doação) e alguns ajustes em outras telas.

--- doaweb/src/main/java/br/unifor/pin/doaweb/manager/doador/ListDoacRealiManager.java
+++ doaweb/src/main/java/br/unifor/pin/doaweb/manager/doador/ListDoacRealiManager.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.unifor.pin.doaweb.bussines.DoacaoBO;
+import br.unifor.pin.doaweb.entity.Campanhas;
 import br.unifor.pin.doaweb.entity.Doacao;
+import br.unifor.pin.doaweb.entity.Usuarios;
 import br.unifor.pin.doaweb.to.SegurancaTO;
 import br.unifor.pin.doaweb.utils.Navigation;
 
@@ -22,12 +24,27 @@ public class ListDoacRealiManager {
 	private DoacaoBO doacaoBO;
 	@Autowired
 	private SegurancaTO segurancaTO;
-
+	
+	private String nomeDoador;
+	private String telefoneDoador;
+	private String endDoador;
+	private String obsDoDoaodr;
+	
+	
 	private List<Doacao> ltDoacoes;
 
 	public String listarMinhasDoacRealizadas() {
 		ltDoacoes = doacaoBO.buscarDoacPorDoad(segurancaTO.getUsuario());
 		return Navigation.LISTDOACDOAD;
+	}
+	
+	public String preparaInforDoDoador(Doacao doacao){
+		nomeDoador = doacao.getDoador().getNome();
+		telefoneDoador = doacao.getDoador().getTelDoador();
+		obsDoDoaodr = doacao.getInformacoes();
+		endDoador = doacao.getDoador().getEndereco();
+		
+		return Navigation.SUCESSO;
 	}
 
 	public List<Doacao> getLtDoacoes() {
@@ -38,4 +55,39 @@ public class ListDoacRealiManager {
 		this.ltDoacoes = ltDoacoes;
 	}
 
+	public String getNomeDoador() {
+		return nomeDoador;
+	}
+
+	public void setNomeDoador(String nomeDoador) {
+		this.nomeDoador = nomeDoador;
+	}
+
+	public String getTelefoneDoador() {
+		return telefoneDoador;
+	}
+
+	public void setTelefoneDoador(String telefoneDoador) {
+		this.telefoneDoador = telefoneDoador;
+	}
+
+	public String getObsDoDoaodr() {
+		return obsDoDoaodr;
+	}
+
+	public void setObsDoDoaodr(String obsDoDoaodr) {
+		this.obsDoDoaodr = obsDoDoaodr;
+	}
+
+	public String getEndDoador() {
+		return endDoador;
+	}
+
+	public void setEndDoador(String endDoador) {
+		this.endDoador = endDoador;
+	}
+
+	
+	
+	
 }
